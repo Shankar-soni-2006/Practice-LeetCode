fa02d1f271800f143bec4e66122bc17b40be750c
Shankar Soni <soniyashwant282@gmail.com>
2026-07-04 08:40:05 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (100.00%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -13,10 +13,10 @@ class Solution {
         int val = image[sr][sc];
         if(color == val) return image;
         dfs(image,sr,sc,color,val);
+        System.gc();
         return image;
     }
     static {
-        System.gc();
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             try (java.io.FileWriter fw =
                      new java.io.FileWriter("display_runtime.txt")) {
