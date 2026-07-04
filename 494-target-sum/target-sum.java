02ff87ffde5daea02303b0b45dbfefc1a93979f5
Shankar Soni <soniyashwant282@gmail.com>
2026-07-04 09:55:12 +0530

    Time: 499 ms (28.17%) | Memory: 43 MB (79.32%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,30 +1,21 @@
-class Solution {
-    static int count = 0;
-    public void helper(int[] nums, int target, int sum, int idx){
-        if(idx == nums.length){
-            if(sum==target){
-                count++;
-            }
-            return;
-        } 
-        helper(nums, target, sum+nums[idx],idx+1);
-        helper(nums, target, sum-nums[idx],idx+1);
-    }
+class Solution { 
+    public int helper(int[] nums, int targetSum, int curr, int idx) {
+        if (idx == nums.length) {
+            return curr == targetSum ? 1 : 0;
+        }
+        int x = helper(nums, targetSum, curr + nums[idx], idx + 1);
+        int y = helper(nums, targetSum, curr, idx + 1);
+        return x+y;
+    } 
+
     public int findTargetSumWays(int[] nums, int target) {
-        int n = nums.length;
-        int sum = 0;
-        count = 0;
-        helper(nums,target,sum,0);
-        return count;
-    }
-    static {
-        System.gc();
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (java.io.FileWriter fw =
-                     new java.io.FileWriter("display_runtime.txt")) {
-                fw.write("0");
-            } catch (Exception e) {
-            }
-        }));
-    }
+        int totalsum = 0;
+        for (int i : nums) totalsum += i;
+        if ((totalsum + target) < 0 || (totalsum + target) % 2 != 0 || Math.abs(target) > totalsum) {
+            return 0; 
+        }
+        
+        int targetSum = (totalsum + target) / 2;
+        return helper(nums, targetSum, 0, 0);
+    } 
 }
