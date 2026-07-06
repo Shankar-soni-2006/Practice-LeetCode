2ec1b4c849288a12beca9309bec870eeef2acc15
Shankar Soni <soniyashwant282@gmail.com>
2026-07-06 09:17:27 +0530

    Time: 1 ms (96.32%) | Memory: 42.5 MB (16.92%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.java
+++ 52-n-queens-ii/n-queens-ii.java
@@ -1,12 +1,30 @@
 class Solution {
+    public void helper(List<List<String>> ans,int r, int n, char[][] queen, boolean[]col, boolean[] diag1, boolean[] diag2){
+        if(r==n){
+            List<String> temp = new ArrayList<>();
+            for(char[] ri : queen) temp.add(new String(ri));
+            ans.add(temp);
+            return;
+        }
+        for(int c = 0; c < n; c++){
+            int d1 = n+r-c;
+            int d2 = r+c;
+            if(!col[c] && !diag1[d1] && !diag2[d2]){
+                queen[r][c] = 'Q';
+                col[c] = diag1[d1] = diag2[d2] = true;
+                helper(ans, r+1, n, queen, col, diag1, diag2);
+                queen[r][c] = '.';
+                col[c] = diag1[d1] = diag2[d2] = false;
+            }
+        }
+    }
     public int totalNQueens(int n) {
-        if (n == 1) return 1;
-        if (n == 2 || n == 3) return 0;
-        if (n == 4) return 2;
-        if (n == 5) return 10;
-        if (n == 6) return 4;
-        if (n == 7) return 40;
-        if (n == 8)  return 92;
-        return 352;
+        List<List<String>> ans = new ArrayList<>();
+        boolean[] col = new boolean[n];
+        boolean[] diag1 = new boolean[n*2];
+        boolean[] diag2 = new boolean[n*2];
+        char[][] queen = new char[n][n];
+        helper(ans, 0, n,queen, col, diag1, diag2);
+        return ans.size();
     }
 }
