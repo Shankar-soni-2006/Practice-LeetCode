1331fa8bb0bdc95be6e76703890ed11de0bbbace
Shankar Soni <soniyashwant282@gmail.com>
2026-07-06 09:21:13 +0530

    Time: 1 ms (96.32%) | Memory: 41.8 MB (86.02%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.java
+++ 52-n-queens-ii/n-queens-ii.java
@@ -1,9 +1,11 @@
 class Solution {
-    public void helper(List<List<String>> ans,int r, int n, char[][] queen, boolean[]col, boolean[] diag1, boolean[] diag2){
+    static int count = 0;
+    public void helper(int r, int n, char[][] queen, boolean[]col, boolean[] diag1, boolean[] diag2){
         if(r==n){
-            List<String> temp = new ArrayList<>();
-            for(char[] ri : queen) temp.add(new String(ri));
-            ans.add(temp);
+            // List<String> temp = new ArrayList<>();
+            // for(char[] ri : queen) temp.add(new String(ri));
+            // ans.add(temp);
+            count++;
             return;
         }
         for(int c = 0; c < n; c++){
@@ -12,19 +14,20 @@ class Solution {
             if(!col[c] && !diag1[d1] && !diag2[d2]){
                 queen[r][c] = 'Q';
                 col[c] = diag1[d1] = diag2[d2] = true;
-                helper(ans, r+1, n, queen, col, diag1, diag2);
+                helper(r+1, n, queen, col, diag1, diag2);
                 queen[r][c] = '.';
                 col[c] = diag1[d1] = diag2[d2] = false;
             }
         }
     }
     public int totalNQueens(int n) {
-        List<List<String>> ans = new ArrayList<>();
+        // List<List<String>> ans = new ArrayList<>();
+        count = 0;
         boolean[] col = new boolean[n];
         boolean[] diag1 = new boolean[n*2];
         boolean[] diag2 = new boolean[n*2];
         char[][] queen = new char[n][n];
-        helper(ans, 0, n,queen, col, diag1, diag2);
-        return ans.size();
+        helper(0, n,queen, col, diag1, diag2);
+        return count;
     }
 }
