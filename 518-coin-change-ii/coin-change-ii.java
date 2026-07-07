6f5b2f309a145666df30fa96a85453a57f432f50
Shankar Soni <soniyashwant282@gmail.com>
2026-07-07 09:20:02 +0530

    Time: 19 ms (21.80%) | Memory: 57.2 MB (21.69%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    int[][] dp;
+    public int helper(int[] coins, int amount, int idx) {
+        if (amount == 0) return 1;
+        if(amount < 0 || idx < 0) return 0; 
+        if (dp[idx][amount] != -1) return dp[idx][amount]; 
+        int take = helper(coins, amount - coins[idx], idx);
+        int notTake = helper(coins, amount, idx - 1);
+        return dp[idx][amount] = take+notTake;  
+    }
+    public int change(int amount, int[] coins) {
+        if (amount == 0) return 1;
+        if (coins.length == 0) return 0;
+        dp = new int[coins.length][amount+1];
+        for(int[] x : dp) Arrays.fill(x,-1);
+        Arrays.sort(coins);
+        return helper(coins, amount, coins.length - 1);
+        
+    }
+}
