7a99bd0c74a5080291b4c52153db9c821ec560b1
Shankar Soni <soniyashwant282@gmail.com>
2026-07-09 13:01:06 +0530

    Time: 4 ms (38.42%) | Memory: 43.7 MB (10.41%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st  = new Stack<>();
         if(s.length()%2!=0) return false;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)=='[') st.push(s.charAt(i));
+        Stack<Character> st = new Stack<>();
+        st.push(s.charAt(0));
+        for(int i = 1;i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '[') st.push(s.charAt(i));
             else{
-                if(st.size()!=0 && st.peek()=='(' && s.charAt(i)==')') st.pop();
-                else if(st.size()!=0 && st.peek()=='{' && s.charAt(i)=='}') st.pop();
-                else if(st.size()!=0 && st.peek()=='[' && s.charAt(i)==']') st.pop();
+                if(!st.isEmpty() && st.peek() == '(' && ch == ')') st.pop();
+                else if(!st.isEmpty() && st.peek() == '[' && ch == ']') st.pop();
+                else if(!st.isEmpty() && st.peek() == '{' && ch == '}') st.pop();
                 else return false;
-            }    
+            }
         }
-        if(st.size()!=0){
-            return false;
-        }
+        if(st.size()!=0) return false;
         return true;
     }
 }
