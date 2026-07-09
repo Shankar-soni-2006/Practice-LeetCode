9fac4bfb8c8102e5255490f042ee1425a4edcaa6
Shankar Soni <soniyashwant282@gmail.com>
2026-07-09 19:41:45 +0530

    Time: 216 ms (66.73%) | Memory: 43 MB (34.45%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -2,10 +2,9 @@ class Solution {
     public boolean valid(char[][]board, int r, int c, int digit){
         for(int i = 0; i < 9; i++){
             if(board[r][i] == digit) return false;
-        }
-        for(int i = 0; i < 9; i++){
             if(board[i][c] == digit) return false;
         }
+        
         int sr = (r/3) * 3;
         int sc = (c/3) * 3;
         for(int i = 0; i < 3; i++){
