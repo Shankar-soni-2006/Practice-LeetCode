cbb337f9a447a9be9904690ceda3a926ba9eea8a
Shankar Soni <soniyashwant282@gmail.com>
2026-08-17 10:46:48 +0530

    Time: 1 ms (56.00%) | Memory: 42.5 MB (98.40%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -1,23 +1,26 @@
 class Solution {
-    public boolean isPossible(int[] nums,int k,int sumLimit){
-        int subArray=1;
-        int currSum=0;
-        for(int i:nums){
-            if(i>sumLimit) return false;
-            if(currSum+i<=sumLimit) currSum+=i;
-            else{
-                subArray++;
-                currSum=i;
+    public boolean isPossible(int[] nums,int k,int mid){
+        int split = 0, curr = 0;
+        for(int i = 0;i < nums.length; i++){
+            if(curr < nums[i]){
+                split++;
+                curr = mid;
+                curr-=nums[i];
+            }else{
+                curr-=nums[i];
             }
         }
-        if(subArray>k) return false;
-        return true;
+        return split <= k;
     }
-    public int splitArray(int[] nums, int k) {
+    public int splitArray(int[] nums, int k){
         if(nums.length<k) return -1;
         int left=0;
+        
         int right=0;
-        for(int i:nums) right+=i;
+        for(int i:nums){
+            right+=i;
+            left = Math.max(left, i);
+        }
         while(left<=right){
             int mid=left+(right-left)/2;
             if(isPossible(nums,k,mid)) right=mid-1;
