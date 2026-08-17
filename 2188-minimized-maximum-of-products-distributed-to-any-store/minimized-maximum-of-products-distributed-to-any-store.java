f232013eb7ef45a0789ce8ba41fbe803c939d3e4
Shankar Soni <soniyashwant282@gmail.com>
2026-08-17 12:48:38 +0530

    Time: 23 ms (87.86%) | Memory: 80.9 MB (94.40%) - LeetSync

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isChecker(int[] q, int k, int mid){
+        if(mid == 0) return false;
+        int cnt = 0;
+        for(int i : q){
+            cnt+=((i+mid-1) / mid); 
+        }
+        return cnt <= k;
+    }
+    public int minimizedMaximum(int k, int[] q) {
+        int n = q.length;
+        int l = 0, r = 0;
+        for(int x : q) r = Math.max(r, x);    
+        while(l <= r){
+            int mid = l+(r-l)/2;
+            if(isChecker(q, k, mid)) r = mid-1;
+            else l = mid+1;
+        }
+        return l;
+    }
+}
