31328f1da7d85d3b45d85b7e84b7e924e50e8faf
Shankar Soni <soniyashwant282@gmail.com>
2026-08-21 08:46:52 +0530

    Time: 5 ms (98.71%) | Memory: 44 MB (42.12%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
         int[] mapS = new int[256];
-        int[] mapT = new int[256];
-        
+        int[] mapT = new int[256];     
         for(int i = 0; i < s.length(); i++){
              char charS = s.charAt(i);
              char charT = t.charAt(i);
